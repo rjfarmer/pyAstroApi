1438b650c511e2d0c5c80f00bb8c5148ebe02c64
Robert Farmer <r.j.farmer@uva.nl>
2021-10-23 18:50:17 +0200

    Fix importlib for old python

--- src/pyadsapi/__init__.py
+++ src/pyadsapi/__init__.py
@@ -1,7 +1,10 @@
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
 
 try:
-    __version__ = version("pyadsapi")
-except PackageNotFoundError:
+    __version__ = metadata.version("pyadsapi")
+except metadata.PackageNotFoundError:
     # package is not installed
     pass
