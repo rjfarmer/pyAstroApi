e07d54c4b24ec83508086bcf77e6d806334f1639
Rob Farmer <robert.j.farmer37@gmail.com>
2022-08-02 17:45:07 -0700

    Add empty notimplementatederrors

--- src/pyastroapi/api/notifications.py
+++ src/pyastroapi/api/notifications.py
@@ -1,1 +1,25 @@
 # SPDX-License-Identifier: BSD-3-Clause
+
+
+def create():
+    raise NotImplementedError
+
+
+def delete():
+    raise NotImplementedError
+
+
+def edit():
+    raise NotImplementedError
+
+
+def query():
+    raise NotImplementedError
+
+
+def view_all():
+    raise NotImplementedError
+
+
+def view():
+    raise NotImplementedError
--- src/pyastroapi/api/stored.py
+++ src/pyastroapi/api/stored.py
@@ -1,1 +1,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
+
+
+def query():
+    raise NotImplementedError
+
+
+def query2svg():
+    raise NotImplementedError
+
+
+def save():
+    raise NotImplementedError
+
+
+def search():
+    raise NotImplementedError
