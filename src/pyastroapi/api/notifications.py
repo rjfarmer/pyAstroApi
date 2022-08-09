d0f9e4cc69d29549e59cd31ab09e0ee0759cabb4
Rob Farmer <robert.j.farmer37@gmail.com>
2022-08-09 12:26:05 -0700

    Start on notifications

--- src/pyastroapi/api/notifications.py
+++ src/pyastroapi/api/notifications.py
@@ -1,25 +1,73 @@
 # SPDX-License-Identifier: BSD-3-Clause
+from . import exceptions as e
+from . import urls
+from . import http
+from . import utils
 
 
-def create():
+def create_template(
+    token: str,
+    name: str = "",
+    active: bool = True,
+    frequnecy="daily",
+    template: str = "arxiv",
+    classes=None,
+    data: str = "",
+):
     raise NotImplementedError
 
 
-def delete():
+def create_query(
+    token: str,
+    name: str = "",
+    qid: str = "",
+    active: bool = True,
+    frequnecy="daily",
+    data: str = "",
+):
     raise NotImplementedError
+
+
+def delete(token: str, myads_id: str):
+    url = urls.make_url(urls.urls["notification"]["get"], myads_id)
+    r = http.delete(token, url)
+
+    if r.status != 200:
+        raise e.AdsApiError(r.response["error"])
+
+    return r.response
 
 
 def edit():
     raise NotImplementedError
 
 
-def query():
-    raise NotImplementedError
+def query(token: str, myads_id: str):
+    url = urls.make_url(urls.urls["notification"]["get"], myads_id)
+    r = http.get(token, url, {}, json=False)
+
+    if r.status != 200:
+        raise e.AdsApiError(r.response["error"])
+
+    return r.response
 
 
-def view_all():
-    raise NotImplementedError
+def view_all(token: str):
+
+    url = urls.make_url(urls.urls["notification"]["edit"])
+    r = http.get(token, url, {}, json=False)
+
+    if r.status != 200:
+        raise e.AdsApiError(r.response["error"])
+
+    return r.response
 
 
-def view():
-    raise NotImplementedError
+def view(token: str, myads_id: str):
+    url = urls.make_url(urls.urls["notification"]["edit"], myads_id)
+    r = http.get(token, url, {}, json=False)
+
+    if r.status != 200:
+        raise e.AdsApiError(r.response["error"])
+
+    return r.response
